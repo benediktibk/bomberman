46ee77678fce28f67b0a0d6a9ff69fee53d92051
Benedikt Schmidt <benediktibk@aon.at>
2013-05-22 13:37:13 +0200

    created b2 world

--- physic/incl/physicsimulator.h
+++ physic/incl/physicsimulator.h
@@ -1,11 +1,20 @@
 #ifndef PHYSICSIMULATOR_H
 #define PHYSICSIMULATOR_H
 
+class b2World;
+class b2Vec2;
+
 namespace Physic
 {
 class PhysicSimulator
 {
+public:
+	PhysicSimulator();
+	~PhysicSimulator();
 
+private:
+	b2Vec2 *m_gravity;
+	b2World *m_world;
 };
 }
 
--- physic/src/physicsimulator.cpp
+++ physic/src/physicsimulator.cpp
@@ -1,1 +1,17 @@
 #include "physicsimulator.h"
+#include "Box2D/Box2D.h"
+
+using namespace Physic;
+
+PhysicSimulator::PhysicSimulator() :
+	m_gravity(new b2Vec2(0, -9.81)),
+	m_world(new b2World(*m_gravity))
+{
+	m_world->SetAllowSleeping(true);
+}
+
+PhysicSimulator::~PhysicSimulator()
+{
+	delete m_world;
+	delete m_gravity;
+}
