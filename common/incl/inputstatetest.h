44bd3c3eab8e8e8b282589f93363a26cd4698b08
Florian Wirnshofer <florian.wirnshofer@hotmail.de>
2013-05-22 20:23:48 +0200

    Added InputState Tests for left right and down

--- common/incl/inputstatetest.h
+++ common/incl/inputstatetest.h
@@ -9,16 +9,35 @@ namespace Common
 class InputStateTest :
 		public CPPUNIT_NS::TestFixture
 {
+
 	 CPPUNIT_TEST_SUITE(InputStateTest);
-	 CPPUNIT_TEST(constructor_empty_upKeyIsNotPressed);
+     CPPUNIT_TEST(constructor_empty_upKeyIsNotPressed);
+     CPPUNIT_TEST(constructor_empty_downKeyIsNotPressed);
+     CPPUNIT_TEST(constructor_empty_leftKeyIsNotPressed);
+     CPPUNIT_TEST(constructor_empty_rightKeyIsNotPressed);
 	 CPPUNIT_TEST(setUpKeyPressed_empty_upKeyIsPressed);
+     CPPUNIT_TEST(setDownKeyPressed_empty_downKeyIsPressed);
+     CPPUNIT_TEST(setLeftKeyPressed_empty_leftKeyIsPressed);
+     CPPUNIT_TEST(setRightKeyPressed_empty_rightKeyIsPressed);
 	 CPPUNIT_TEST(setUpKeyNotPressed_previouslyPressed_upKeyIsNotPressed);
+     CPPUNIT_TEST(setDownKeyNotPressed_previouslyPressed_downKeyIsNotPressed);
+     CPPUNIT_TEST(setLeftKeyNotPressed_previouslyPressed_leftKeyIsNotPressed);
+     CPPUNIT_TEST(setRightKeyNotPressed_previouslyPressed_rightKeyIsNotPressed);
 	 CPPUNIT_TEST_SUITE_END();
 
 private:
 	 void constructor_empty_upKeyIsNotPressed();
 	 void setUpKeyPressed_empty_upKeyIsPressed();
-	 void setUpKeyNotPressed_previouslyPressed_upKeyIsNotPressed();
+     void setUpKeyNotPressed_previouslyPressed_upKeyIsNotPressed();
+     void constructor_empty_downKeyIsNotPressed();
+     void setDownKeyPressed_empty_downKeyIsPressed();
+     void setDownKeyNotPressed_previouslyPressed_downKeyIsNotPressed();
+     void constructor_empty_leftKeyIsNotPressed();
+     void setLeftKeyPressed_empty_leftKeyIsPressed();
+     void setLeftKeyNotPressed_previouslyPressed_leftKeyIsNotPressed();
+     void constructor_empty_rightKeyIsNotPressed();
+     void setRightKeyPressed_empty_rightKeyIsPressed();
+     void setRightKeyNotPressed_previouslyPressed_rightKeyIsNotPressed();
 };
 }
 
--- common/src/inputstatetest.cpp
+++ common/src/inputstatetest.cpp
@@ -10,6 +10,27 @@ void InputStateTest::constructor_empty_upKeyIsNotPressed()
 	CPPUNIT_ASSERT(!inputState.isUpKeyPressed());
 }
 
+void InputStateTest::constructor_empty_downKeyIsNotPressed()
+{
+    InputState inputState;
+
+    CPPUNIT_ASSERT(!inputState.isDownKeyPressed());
+}
+
+void InputStateTest::constructor_empty_leftKeyIsNotPressed()
+{
+    InputState inputState;
+
+    CPPUNIT_ASSERT(!inputState.isLeftKeyPressed());
+}
+
+void InputStateTest::constructor_empty_rightKeyIsNotPressed()
+{
+    InputState inputState;
+
+    CPPUNIT_ASSERT(!inputState.isRightKeyPressed());
+}
+
 void InputStateTest::setUpKeyPressed_empty_upKeyIsPressed()
 {
 	InputState inputState;
@@ -19,6 +40,34 @@ void InputStateTest::setUpKeyPressed_empty_upKeyIsPressed()
 	CPPUNIT_ASSERT(inputState.isUpKeyPressed());
 }
 
+void InputStateTest::setDownKeyPressed_empty_downKeyIsPressed()
+{
+    InputState inputState;
+
+    inputState.setDownKeyPressed();
+
+    CPPUNIT_ASSERT(inputState.isDownKeyPressed());
+}
+
+void InputStateTest::setLeftKeyPressed_empty_leftKeyIsPressed()
+{
+    InputState inputState;
+
+    inputState.setLeftKeyPressed();
+
+    CPPUNIT_ASSERT(inputState.isLeftKeyPressed());
+}
+
+void InputStateTest::setRightKeyPressed_empty_rightKeyIsPressed()
+{
+    InputState inputState;
+
+    inputState.setRightKeyPressed();
+
+    CPPUNIT_ASSERT(inputState.isRightKeyPressed());
+}
+
+
 void InputStateTest::setUpKeyNotPressed_previouslyPressed_upKeyIsNotPressed()
 {
 	InputState inputState;
@@ -27,4 +76,34 @@ void InputStateTest::setUpKeyNotPressed_previouslyPressed_upKeyIsNotPressed()
 	inputState.setUpKeyNotPressed();
 
 	CPPUNIT_ASSERT(!inputState.isUpKeyPressed());
+}
+
+void InputStateTest::setDownKeyNotPressed_previouslyPressed_downKeyIsNotPressed()
+{
+    InputState inputState;
+    inputState.setDownKeyPressed();
+
+    inputState.setDownKeyNotPressed();
+
+    CPPUNIT_ASSERT(!inputState.isDownKeyPressed());
+}
+
+void InputStateTest::setLeftKeyNotPressed_previouslyPressed_leftKeyIsNotPressed()
+{
+    InputState inputState;
+    inputState.setLeftKeyPressed();
+
+    inputState.setLeftKeyNotPressed();
+
+    CPPUNIT_ASSERT(!inputState.isLeftKeyPressed());
+}
+
+void InputStateTest::setRightKeyNotPressed_previouslyPressed_rightKeyIsNotPressed()
+{
+    InputState inputState;
+    inputState.setRightKeyPressed();
+
+    inputState.setRightKeyNotPressed();
+
+    CPPUNIT_ASSERT(!inputState.isRightKeyPressed());
 }
