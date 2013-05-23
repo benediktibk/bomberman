4f26d91c0ecd22b712011a1a663f8d8204cf269b
Benedikt Schmidt <benediktibk@aon.at>
2013-05-23 16:00:35 +0200

    made player completely black

--- graphic/src/player.cpp
+++ graphic/src/player.cpp
@@ -1,14 +1,19 @@
 #include "player.h"
 #include "playerstate.h"
 #include <QGraphicsRectItem>
+#include <QGraphicsScene>
 
 using namespace Graphic;
 using namespace Common;
 
 Player::Player(QGraphicsScene &scene) :
-	m_item(new QGraphicsRectItem(0, &scene))
+	m_item(new QGraphicsRectItem(0))
 {
 	m_item->setRect(-10, -10, 20, 20);
+	m_item->setBrush(QBrush(QColor(0, 0, 0)));
+	m_item->setPen(QPen(QColor(0, 0, 0)));
+
+	scene.addItem(m_item);
 }
 
 Player::~Player()
