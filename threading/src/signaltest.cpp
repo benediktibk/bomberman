a06cc28a8461542eec0f1dc71510842ac6da59e2
Florian Wirnshofer <florian.wirnshofer@hotmail.de>
2013-05-27 18:45:09 +0200

    avoided possible race condition

--- threading/src/signaltest.cpp
+++ threading/src/signaltest.cpp
@@ -11,12 +11,19 @@ public:
 	ThreadSignalTestImplementation(Signal &signalValueSet, double &source, double &destination) :
 		m_signalValueSet(signalValueSet),
 		m_source(source),
-		m_destination(destination)
+        m_destination(destination),
+        m_waitingStarted(false)
 	{ }
+
+    bool waitingStarted()
+    {
+        return m_waitingStarted;
+    }
 
 private:
 	virtual void execute()
 	{
+        m_waitingStarted = true;
 		m_signalValueSet.wait();
 		m_destination = m_source;
 	}
@@ -25,6 +32,7 @@ private:
 	Signal &m_signalValueSet;
 	double &m_source;
 	double &m_destination;
+    bool m_waitingStarted;
 };
 
 void SignalTest::send_threadWaitingOnSignalAndThenCopiesValue_destinationHasValueSetAfterCreationOfThread()
@@ -33,6 +41,9 @@ void SignalTest::send_threadWaitingOnSignalAndThenCopiesValue_destinationHasValu
 	double destination = 0;
 	Signal signalValueSet;
 	ThreadSignalTestImplementation thread(signalValueSet, source, destination);
+
+    while (!thread.waitingStarted())
+        usleep(1000);
 
 	source = 5;
 	signalValueSet.send();
