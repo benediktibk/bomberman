e5b59a98cdbdf6a615346f29203388a4de00d34f
Florian Wirnshofer <florian.wirnshofer@hotmail.de>
2013-05-27 21:02:46 +0200

    Added Unique Id Creator test

---
+++ common/incl/uniqueidcreatortest.h
@@ -0,0 +1,26 @@
+#ifndef UNIQUEIDCREATORTEST_H
+#define UNIQUEIDCREATORTEST_H
+
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+namespace Common
+{
+class UniqueIdCreatorTest :
+        public CPPUNIT_NS::TestFixture
+{
+     CPPUNIT_TEST_SUITE(UniqueIdCreatorTest);
+     CPPUNIT_TEST(getId_when_third_deleted());
+     CPPUNIT_TEST(getId_when_third());
+     CPPUNIT_TEST(getId_when_first());
+     CPPUNIT_TEST_SUITE_END();
+
+private:
+     void getId_when_third_deleted();
+     void getId_when_third();
+     void getId_when_first();
+};
+}
+
+#endif
+
