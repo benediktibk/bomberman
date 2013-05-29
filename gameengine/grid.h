96a6b7846ddbccc8cb7b2984672ef9172407cb4f
Florian Wirnshofer <florian.wirnshofer@hotmail.de>
2013-05-29 11:49:06 +0200

    Added functions add/removewallatplace  add/removebombatplace as well as a function getVectorIndex which returns the matrix index of a given point position

--- gameengine/grid.h
+++ gameengine/grid.h
@@ -1,10 +1,16 @@
 #ifndef GRID_H
 #define GRID_H
 
-
+#include <vector>
+#include "common/point.h"
+#include "common/bombstate.h"
+#include "common/wallstate.h"
+#include "common/playerstate.h"
+namespace GameEngine
+{
 class Grid
 {
-public:
+private:
     enum Item
     {
         ItemWall,
@@ -13,13 +19,25 @@ public:
         ItemFree
     };
 
+public:
+    Grid(double width,double height);
+
+    bool isPlaceEmpty(const Common::Point &position) const;
+    void addBombAtPlace(const Common::BombState &bomb);
+    void addWallAtPlace(const Common::WallState &wall);
+    void removeBomb(const Common::BombState &bomb);
+    void removeWall(const Common::WallState &wall);
+    void updatePlayer(const Common::PlayerState &player);
+
 private:
-    unsigned int m_gridrows;
-    unsigned int m_gridcolums;
-
+    unsigned int m_gridRows;
+    unsigned int m_gridColumns;
+    unsigned int m_numberOfItems;
+    std::vector<Item> m_itemMatrix;
+    std::vector<unsigned int> m_idMatrix;
+    unsigned int getVectorIndex(const Common::Point &position) const;
 };
-
+}
 
 #endif
-
 
