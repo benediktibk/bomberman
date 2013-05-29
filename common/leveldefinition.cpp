bf14707235ad4f53335da88947bdc75b4ef75d09
Markus Hofbauer <markus.hofbauer@tum.de>
2013-05-29 16:06:54 +0200

    added getobjecttypeatposition in level definition

--- common/leveldefinition.cpp
+++ common/leveldefinition.cpp
@@ -4,12 +4,12 @@ using namespace Common;
 
 LevelDefinition::LevelDefinition() :
     m_width(50),
-    m_height(50)
-{
-}
-/*
-LevelDefinition::getObjectTypeAtPosition(unsigned int position)
-{
+    m_height(50),
+    m_objectMatrix(m_width*m_height,ObjectTypeEmpty)
+{ }
 
+LevelDefinition::ObjectType LevelDefinition::getObjectTypeAtPosition(unsigned int positionX, unsigned int positionY)
+{
+    return m_objectMatrix[m_width*(positionY-1)+positionX-1];
 }
-*/
+
