4667ded827a29d7b9ab9976a601562da00b90411
Florian Wirnshofer <florian.wirnshofer@hotmail.de>
2013-05-29 10:13:02 +0200

    added enum

--- gameengine/grid.h
+++ gameengine/grid.h
@@ -1,4 +1,25 @@
 #ifndef GRID_H
 #define GRID_H
 
-#endif // GRID_H
+
+class Grid
+{
+public:
+    enum Item
+    {
+        ItemWall,
+        ItemBomb,
+        ItemPlayer,
+        ItemFree
+    };
+
+private:
+    unsigned int m_gridrows;
+    unsigned int m_gridcolums;
+
+};
+
+
+#endif
+
+
