3489921c9e48acfa725b4073f73c3ef9f8d2cec4
Benedikt Schmidt <benediktibk@aon.at>
2013-05-29 21:09:42 +0200

    completed checkIncludeGuards.sh

--- checkIncludeGuards.sh
+++ checkIncludeGuards.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-headerFiles=$(find . -path ./physic/Box2D -prune -o -iname "*.h")
+headerFiles=$(find . -path ./physic/Box2D -prune -o -path ./build -prune -o -iname "*.h")
 
-for headerFile in ${headerFiles}
+for headerFileWithPath in ${headerFiles}
 do
-	if [ $headerFile != "./physic/Box2D" ]; then
-		firstLine=$(head --lines 1 ${headerFile})
-		secondLine=$(head --lines 2 ${headerFile} | tail --lines 1)
+	if	[ $headerFileWithPath != "./physic/Box2D" ] &&
+		[ $headerFileWithPath != "./build" ]; then
+		echo "checking ${headerFileWithPath}"
+
+		firstLine=$(head --lines 1 ${headerFileWithPath})
+		secondLine=$(head --lines 2 ${headerFileWithPath} | tail --lines 1)
 		ifndef=${firstLine:0:7}
 		define=${secondLine:0:7}
 
-		if [ $ifndef != "#ifndef" ]; then
-			echo "wrong value in first line of file ${headerFile}: ${ifndef}"
+		if [ "${ifndef}" != "#ifndef" ]; then
+			echo "wrong value in first line: ${ifndef}"
 			exit 1
 		fi
 
 		if [ $define != "#define" ]; then
-			echo "wrong value in first line of file ${headerFile}: ${ifndef}"
+			echo "wrong value in first line: ${ifndef}"
 			exit 1
 		fi
 
@@ -24,13 +27,23 @@ do
 		includeGuardTwo=${secondLine:8:${#secondLine}}
 
 		if [ $includeGuardOne != $includeGuardTwo ]; then
-			echo "the include guards do not match in file ${headerFile}"
+			echo "the include guards do not match"
 			exit 1
 		fi
 
-		includeGuard=$includeGuardOne;
+		includeGuardReal=$includeGuardOne;
 
-		echo ${firstLine}
-		echo ${secondLine}
+		headerFileWithoutPath=${headerFileWithPath##*/}
+		headerFile=${headerFileWithoutPath%.h}
+		path=${headerFileWithPath%/*}
+		subProject=${path##*/}
+		subProjectUpperCase=$(echo $subProject | tr  "[:lower:]" "[:upper:]")
+		fileNameUpperCase=$(echo $headerFile | tr  "[:lower:]" "[:upper:]")
+		includeGuardShouldBe="${subProjectUpperCase}_${fileNameUpperCase}_H"
+
+		if [ $includeGuardReal != $includeGuardShouldBe ]; then
+			echo "the include guards do not match the specified pattern"
+			exit 1
+		fi
 	fi
 done
