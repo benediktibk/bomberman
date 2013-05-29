faf83f51873a03197ff7926f5eab533418dc8d94
Benedikt Schmidt <benediktibk@aon.at>
2013-05-29 13:56:34 +0200

    cleaned up Grid

--- gameengine/grid.h
+++ gameengine/grid.h
@@ -6,36 +6,39 @@
 #include "common/bombstate.h"
 #include "common/wallstate.h"
 #include "common/playerstate.h"
+
 namespace GameEngine
 {
 class Grid
 {
 private:
-    enum Item
-    {
-        ItemWall,
-        ItemBomb,
-        ItemPlayer,
-        ItemFree
-    };
+	enum Item
+	{
+		ItemWall,
+		ItemBomb,
+		ItemPlayer,
+		ItemFree
+	};
 
 public:
-    Grid(double width,double height);
+	Grid(double width,double height);
 
-    bool isPlaceEmpty(const Common::Point &position) const;
-    void addBombAtPlace(const Common::BombState &bomb);
-    void addWallAtPlace(const Common::WallState &wall);
-    void removeBomb(const Common::BombState &bomb);
-    void removeWall(const Common::WallState &wall);
-    void updatePlayer(const Common::PlayerState &player);
+	bool isPlaceEmpty(const Common::Point &position) const;
+	void addBombAtPlace(const Common::BombState &bomb);
+	void addWallAtPlace(const Common::WallState &wall);
+	void removeBomb(const Common::BombState &bomb);
+	void removeWall(const Common::WallState &wall);
+	void updatePlayer(const Common::PlayerState &player);
 
 private:
-    unsigned int m_gridRows;
-    unsigned int m_gridColumns;
-    unsigned int m_numberOfItems;
-    std::vector<Item> m_itemMatrix;
-    std::vector<unsigned int> m_idMatrix;
-    unsigned int getVectorIndex(const Common::Point &position) const;
+	unsigned int getVectorIndex(const Common::Point &position) const;
+
+private:
+	unsigned int m_gridRows;
+	unsigned int m_gridColumns;
+	unsigned int m_numberOfItems;
+	std::vector<Item> m_itemMatrix;
+	std::vector<unsigned int> m_idMatrix;
 };
 }
 
