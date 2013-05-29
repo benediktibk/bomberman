1036e0de822419b6d4083db7ea990ea90209353b
Kevin Meyer <meyer.kevin@mytum.de>
2013-05-29 10:16:26 +0200

    added Unit Tests for WallTypes.

--- common/wallstatetest.cpp
+++ common/wallstatetest.cpp
@@ -13,3 +13,19 @@ void WallStateTest::setPosition_5And4_positionIs5And4()
 
 	CPPUNIT_ASSERT_EQUAL(Point(5, 4), wall.getPosition());
 }
+
+void WallStateTest::construct_looseWall_typeIsLooseWall()
+{
+    UniqueIdCreator id;
+    WallState wall(id, WallState::WallTypeLoose);
+
+    CPPUNIT_ASSERT_EQUAL(WallState::WallTypeLoose, wall.getWallType());
+}
+
+void WallStateTest::construct_solidWall_typeIsSolidWall()
+{
+    UniqueIdCreator id;
+    WallState wall(id, WallState::WallTypeSolid);
+
+    CPPUNIT_ASSERT_EQUAL(WallState::WallTypeSolid, wall.getWallType());
+}
