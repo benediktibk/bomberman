4be0b59ef1b23c72d6fb6acf55564fdc0942e862
Kevin Meyer <meyer.kevin@mytum.de>
2013-05-30 23:55:19 +0200

    added basic tests for MaxBomb PowerUp.

--- common/powerupmaxbombstatetest.h
+++ common/powerupmaxbombstatetest.h
@@ -10,8 +10,13 @@ class PowerUpMaxBombStateTest :
         public CPPUNIT_NS::TestFixture
 {
     CPPUNIT_TEST_SUITE(PowerUpMaxBombStateTest);
+    CPPUNIT_TEST(getPowerUpWidth_widthIs1);
+    CPPUNIT_TEST(getPowerUpHeight_heightIs1);
     CPPUNIT_TEST_SUITE_END();
+
 private:
+    void getPowerUpWidth_widthIs1();
+    void getPowerUpHeight_heightIs1();
 
 };
 }
