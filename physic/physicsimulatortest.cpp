d3c23d2e15d0c30ed56dcb97d804d2868c852872
Benedikt Schmidt <benediktibk@aon.at>
2013-05-30 09:32:11 +0200

    added unit tests for collisions

--- physic/physicsimulatortest.cpp
+++ physic/physicsimulatortest.cpp
@@ -1,6 +1,7 @@
 #include "physicsimulatortest.h"
 #include "physicsimulator.h"
 #include "dynamicobject.h"
+#include "staticobject.h"
 
 using namespace Physic;
 using namespace Common;
@@ -29,4 +30,68 @@ void Physic::PhysicSimulatorTest::simulateStep_timeAboveMaximumStepTimeAndLinear
 	Point positionShouldBe(9.9, 6.6);
 	Point positionReal(object.getPosition());
 	CPPUNIT_ASSERT(positionShouldBe.fuzzyEqual(positionReal, 0.0001));
+}
+
+void PhysicSimulatorTest::simulateStep_collisionOfDynamicWithStaticObject_dynamicObjectPositionIsCorrect()
+{
+	PhysicSimulator simulator;
+	StaticObject *staticObject = new StaticObject(simulator, Point(3, 0), 1, 1);
+	DynamicObject *dynamicObject = new DynamicObject(simulator, Point(0, 0), 1, 1);
+	dynamicObject->applyLinearVelocity(10, 0);
+
+	simulator.simulateStep(1);
+
+	Point positionShouldBe(2, 0);
+	Point positionReal(dynamicObject->getPosition());
+	delete staticObject;
+	delete dynamicObject;
+	CPPUNIT_ASSERT(positionShouldBe.fuzzyEqual(positionReal, 0.05));
+}
+
+void PhysicSimulatorTest::simulateStep_collisionOfDynamicWithStaticObject_staticObjectPositionIsNotChanged()
+{
+	PhysicSimulator simulator;
+	StaticObject *staticObject = new StaticObject(simulator, Point(3, 0), 1, 1);
+	DynamicObject *dynamicObject = new DynamicObject(simulator, Point(0, 0), 1, 1);
+	dynamicObject->applyLinearVelocity(10, 0);
+
+	simulator.simulateStep(1);
+
+	Point positionShouldBe(3, 0);
+	Point positionReal(staticObject->getPosition());
+	delete staticObject;
+	delete dynamicObject;
+	CPPUNIT_ASSERT(positionShouldBe.fuzzyEqual(positionReal, 0.05));
+}
+
+void PhysicSimulatorTest::simulateStep_collisionOfDynamicWithDynamicObject_firstDynamicObjectPositionIsCorrect()
+{
+	PhysicSimulator simulator;
+	DynamicObject *objectOne = new DynamicObject(simulator, Point(3, 0), 1, 1);
+	DynamicObject *objectTwo = new DynamicObject(simulator, Point(0, 0), 1, 1);
+	objectTwo->applyLinearVelocity(10, 0);
+
+	simulator.simulateStep(1);
+
+	Point positionShouldBe(7, 0);
+	Point positionReal(objectOne->getPosition());
+	delete objectOne;
+	delete objectTwo;
+	CPPUNIT_ASSERT(positionShouldBe.fuzzyEqual(positionReal, 0.05));
+}
+
+void PhysicSimulatorTest::simulateStep_collisionOfDynamicWithDynamicObject_secondDynamicObjectPositionIsCorrect()
+{
+	PhysicSimulator simulator;
+	DynamicObject *objectOne = new DynamicObject(simulator, Point(3, 0), 1, 1);
+	DynamicObject *objectTwo = new DynamicObject(simulator, Point(0, 0), 1, 1);
+	objectTwo->applyLinearVelocity(10, 0);
+
+	simulator.simulateStep(1);
+
+	Point positionShouldBe(6, 0);
+	Point positionReal(objectTwo->getPosition());
+	delete objectOne;
+	delete objectTwo;
+	CPPUNIT_ASSERT(positionShouldBe.fuzzyEqual(positionReal, 0.05));
 }
--- physic/physicsimulatortest.h
+++ physic/physicsimulatortest.h
@@ -12,11 +12,19 @@ class PhysicSimulatorTest :
 	 CPPUNIT_TEST_SUITE(PhysicSimulatorTest);
 	 CPPUNIT_TEST(simulateStep_timeBelowMaximumStepTimeAndLinearMovingObject_objectPositionIsCorrect);
 	 CPPUNIT_TEST(simulateStep_timeAboveMaximumStepTimeAndLinearMovingObject_objectPositionIsCorrect);
+	 CPPUNIT_TEST(simulateStep_collisionOfDynamicWithStaticObject_dynamicObjectPositionIsCorrect);
+	 CPPUNIT_TEST(simulateStep_collisionOfDynamicWithStaticObject_staticObjectPositionIsNotChanged);
+	 CPPUNIT_TEST(simulateStep_collisionOfDynamicWithDynamicObject_firstDynamicObjectPositionIsCorrect);
+	 CPPUNIT_TEST(simulateStep_collisionOfDynamicWithDynamicObject_secondDynamicObjectPositionIsCorrect);
 	 CPPUNIT_TEST_SUITE_END();
 
 private:
 	 void simulateStep_timeBelowMaximumStepTimeAndLinearMovingObject_objectPositionIsCorrect();
 	 void simulateStep_timeAboveMaximumStepTimeAndLinearMovingObject_objectPositionIsCorrect();
+	 void simulateStep_collisionOfDynamicWithStaticObject_dynamicObjectPositionIsCorrect();
+	 void simulateStep_collisionOfDynamicWithStaticObject_staticObjectPositionIsNotChanged();
+	 void simulateStep_collisionOfDynamicWithDynamicObject_firstDynamicObjectPositionIsCorrect();
+	 void simulateStep_collisionOfDynamicWithDynamicObject_secondDynamicObjectPositionIsCorrect();
 };
 }
 
