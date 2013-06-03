5e85dd00c1351c09765f238a202280e0e65282be
Benedikt Schmidt <benediktibk@aon.at>
2013-06-03 23:23:41 +0200

    improved coverage test

--- testCoverage.sh
+++ testCoverage.sh
@@ -5,4 +5,5 @@ sourceDirectory=$1
 find . -iname "*.gcda" -print0 | xargs -0 rm -rf
 ./main/bin/testrunner
 lcov --capture --directory "${sourceDirectory}" --output-file coverage.info --no-external
+lcov --remove coverage.info "physic/Box2D/*" -o coverage.info
 genhtml coverage.info --output-directory html
