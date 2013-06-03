a517c69846a23aab47cfcc7723fe4587c92a50c0
Benedikt Schmidt <benediktibk@aon.at>
2013-06-04 00:19:15 +0200

    avoided unsupported argument on debian of lcov

--- testCoverage.sh
+++ testCoverage.sh
@@ -4,6 +4,7 @@ sourceDirectory=$1
 
 find . -iname "*.gcda" -print0 | xargs -0 rm -rf
 ./main/bin/testrunner
-lcov --capture --directory "${sourceDirectory}" --output-file coverage.info --no-external
+lcov --capture --directory "${sourceDirectory}" --output-file coverage.info
 lcov --remove coverage.info "physic/Box2D/*" -o coverage.info
+lcov --remove coverage.info "/usr/*" -o coverage.info
 genhtml coverage.info --output-directory html
