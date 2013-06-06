8204f7daafb94be0fa8659f1295e860f4ef6c499
Benedikt Schmidt <benediktibk@aon.at>
2013-06-06 09:32:29 +0200

    removed branch data from test coverage

--- testCoverage.sh
+++ testCoverage.sh
@@ -3,8 +3,9 @@
 sourceDirectory=$1
 
 find . -iname "*.gcda" -print0 | xargs -0 rm -rf
+lcov --zerocounters --directory "${sourceDirectory}"
 ./main/bin/testrunner
 lcov --capture --directory "${sourceDirectory}" --output-file coverage.info
 lcov --remove coverage.info "physic/Box2D/*" -o coverage.info
 lcov --remove coverage.info "/usr/*" -o coverage.info
-genhtml coverage.info --output-directory html
+genhtml coverage.info --output-directory html --no-branch-coverage
