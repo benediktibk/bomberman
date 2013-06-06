acf09ff4d489873223d634e375967e636cf76d5c
Benedikt Schmidt <benediktibk@aon.at>
2013-06-06 19:18:34 +0200

    fixed spelling error in function name

--- physic/staticobjecttest.h
+++ physic/staticobjecttest.h
@@ -6,20 +6,20 @@
 
 namespace Physic
 {
-class StaticObjectTest :
-		public CPPUNIT_NS::TestFixture
-{
-	 CPPUNIT_TEST_SUITE(StaticObjectTest);
-	 CPPUNIT_TEST(constructor_position5And4AndHeight1AndWidth2_positionIs5And4);
-	 CPPUNIT_TEST(constructor_validSimulator_simulatorHasBodyCount1);
-	 CPPUNIT_TEST(desturctor_empty_simulatorHasBodyCount0);
-	 CPPUNIT_TEST_SUITE_END();
+	class StaticObjectTest :
+			public CPPUNIT_NS::TestFixture
+	{
+		 CPPUNIT_TEST_SUITE(StaticObjectTest);
+		 CPPUNIT_TEST(constructor_position5And4AndHeight1AndWidth2_positionIs5And4);
+		 CPPUNIT_TEST(constructor_validSimulator_simulatorHasBodyCount1);
+		 CPPUNIT_TEST(desturctor_empty_simulatorHasBodyCount0);
+		 CPPUNIT_TEST_SUITE_END();
 
-private:
-	 void constructor_position5And4AndHeight1AndWidth2_positionIs5And4();
-	 void constructor_validSimulator_simulatorHasBodyCount1();
-	 void desturctor_empty_simulatorHasBodyCount0();
-};
+	private:
+		 void constructor_position5And4AndHeight1AndWidth2_positionIs5And4();
+		 void constructor_validSimulator_simulatorHasBodyCount1();
+		 void desturctor_empty_simulatorHasBodyCount0();
+	};
 }
 
 #endif
