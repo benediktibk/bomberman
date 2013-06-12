9e92b36b36655d7a55a3b045a68a5bd400f7b188
Kevin Meyer <meyer.kevin@mytum.de>
2013-06-12 20:51:36 +0200

    removed bombPackage phantom files.

--- common/powerupbombpackstate.cpp
+++
@@ -1,8 +0,0 @@
-#include "powerupbombpackstate.h"
-
-using namespace Common;
-
-PowerUpBombPackState::PowerUpBombPackState(UniqueIdCreator &creator, const Point &position) :
-    PowerUpState(creator, position),
-    m_BombsInPackage(5)
-{ }
