3a9d2a7960bfd55fe4535c9c6ae810b0dbea7b03
Markus Hofbauer <markus.hofbauer@tum.de>
2013-06-18 19:51:28 +0200

    more unit tests

--- common/csvparsertest.cpp
+++ common/csvparsertest.cpp
@@ -10,3 +10,19 @@ void CSVParserTest::getWidthOfFile_pareFileDefaultlevel_resultIs15()
 
 	CPPUNIT_ASSERT_EQUAL((unsigned int)15, testRead.getWidthOfFile());
 }
+
+void CSVParserTest::getHeightOfFile_pareFileDefaultlevel_resultIs10()
+{
+	CSVParser testRead;
+	testRead.parseFile("defaultlevel");
+
+	CPPUNIT_ASSERT_EQUAL((unsigned int)10, testRead.getHeightOfFile());
+}
+
+void CSVParserTest::getTextInField_position3And2_resultIsLoose()
+{
+	CSVParser testRead;
+	testRead.parseFile("defaultlevel");
+
+	CPPUNIT_ASSERT_EQUAL((std::string)"loose", testRead.getTextInField(3,2));
+}
--- common/csvparsertest.h
+++ common/csvparsertest.h
@@ -12,10 +12,14 @@ class CSVParserTest :
 {
 	CPPUNIT_TEST_SUITE(CSVParserTest);
 	CPPUNIT_TEST(getWidthOfFile_pareFileDefaultlevel_resultIs15);
+	CPPUNIT_TEST(getHeightOfFile_pareFileDefaultlevel_resultIs10);
+	CPPUNIT_TEST(getTextInField_position3And2_resultIsLoose);
 	CPPUNIT_TEST_SUITE_END();
 
 private:
 	void getWidthOfFile_pareFileDefaultlevel_resultIs15();
+	void getHeightOfFile_pareFileDefaultlevel_resultIs10();
+	void getTextInField_position3And2_resultIsLoose();
 
 };
 }
