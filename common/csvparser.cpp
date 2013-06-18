aec187a482e16333d68a35b74f3835d02bdeb8b5
Markus Hofbauer <markus.hofbauer@tum.de>
2013-06-18 14:49:20 +0200

    add first version of csvparser

--- common/csvparser.cpp
+++ common/csvparser.cpp
@@ -1,7 +1,37 @@
 #include "csvparser.h"
 
 using namespace Common;
+using namespace std;
 
 CSVParser::CSVParser()
+{ }
+
+void CSVParser::parseFile(std::string filename)
 {
+	string opneninngFile = "resources/levels/" + filename + ".csv";
+	ifstream csvRead(opneninngFile.c_str());
+	string textInLine;
+
+	if(csvRead.is_open())
+	{
+		while(!csvRead.eof() && csvRead.good())
+		{
+			getline(csvRead, textInLine, ';');
+			m_textInFile.push_back(textInLine);
+		}
+	}
+	else
+		cerr << "ERROR could not open file" << endl;
+
+	csvRead.close();
+}
+
+unsigned int CSVParser::getColumsFromFile() const
+{
+	return m_colums;
+}
+
+unsigned int CSVParser::getRowsFromFile() const
+{
+	return m_rows;
 }
--- common/csvparser.h
+++ common/csvparser.h
@@ -1,12 +1,26 @@
 #ifndef COMMON_CSVPARSER_H
 #define COMMON_CSVPARSER_H
 
+#include <stddef.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
 namespace Common
 {
 class CSVParser
 {
 public:
 	CSVParser();
+	void parseFile(std::string filename);
+	unsigned int getRowsFromFile() const;
+	unsigned int getColumsFromFile() const;
+
+private:
+	std::vector<std::string> m_textInFile;
+	unsigned int m_rows;
+	unsigned int m_colums;
 };
 }
 #endif // CSVPARSER_H
