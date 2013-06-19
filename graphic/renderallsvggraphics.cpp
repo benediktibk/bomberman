95dc5d6563acf8df046d924f28afa7e13d5a076c
Kevin Meyer <meyer.kevin@mytum.de>
2013-06-19 10:14:24 +0200

    added renderAllSvgGraphics class

---
+++ graphic/renderallsvggraphics.cpp
@@ -0,0 +1,8 @@
+#include "renderallsvggraphics.h"
+#include <QtSvg/QtSvg>
+
+using namespace Graphic;
+
+RenderAllSvgGraphics::RenderAllSvgGraphics(const double pixelPerMeter) :
+	m_pixelPerMeter(pixelPerMeter)
+{ }
---
+++ graphic/renderallsvggraphics.h
@@ -0,0 +1,21 @@
+#ifndef GRAPHIC_RENDERALLSVGGRAPHICS_H
+#define GRAPHIC_RENDERALLSVGGRAPHICS_H
+
+class QGraphicsSvgItem;
+
+namespace Graphic
+{
+class RenderAllSvgGraphics
+{
+public:
+	RenderAllSvgGraphics(const double pixelPerMeter);
+	QGraphicsSvgItem* getPlayerItem();
+
+private:
+	const double m_pixelPerMeter;
+	QGraphicsSvgItem *m_playerItem;
+
+};
+}
+
+#endif
