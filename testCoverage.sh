0c2205820929bbecbe5fa3d48edc01bdb4c3e172
Benedikt Schmidt <benediktibk@aon.at>
2013-06-19 21:55:22 +0200

    fixed working directory for coverage check

--- testCoverage.sh
+++ testCoverage.sh
@@ -4,7 +4,9 @@ sourceDirectory=$1
 
 find . -iname "*.gcda" -print0 | xargs -0 rm -rf
 lcov --zerocounters --directory "${sourceDirectory}"
-./main/bin/testrunner
+cd main/bin
+./testrunner
+cd ../..
 lcov --capture --directory "${sourceDirectory}" --output-file coverage.info
 lcov --remove coverage.info "physic/Box2D/*" -o coverage.info
 lcov --remove coverage.info "/usr/*" -o coverage.info
