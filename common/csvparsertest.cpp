389b8695a8e9c8aaa1429821764a6e22f8c93d7f
Markus Hofbauer <markus.hofbauer@tum.de>
2013-06-19 16:36:41 +0200

    renamed testfiles

--- common/csvparsertest.cpp
+++ common/csvparsertest.cpp
@@ -5,28 +5,28 @@ using namespace Common;
 
 void CSVParserTest::getWidthOfFile_pareFileDefaultlevel_resultIs15()
 {
-	CSVParser testRead("testfiles/defaultlevel");
+	CSVParser testRead("testfiles/testlevel");
 
 	CPPUNIT_ASSERT_EQUAL((unsigned int)15, testRead.getWidthOfFile());
 }
 
 void CSVParserTest::getHeightOfFile_pareFileDefaultlevel_resultIs10()
 {
-	CSVParser testRead("testfiles/defaultlevel");
+	CSVParser testRead("testfiles/testlevel");
 
 	CPPUNIT_ASSERT_EQUAL((unsigned int)10, testRead.getHeightOfFile());
 }
 
 void CSVParserTest::getTextInField_position3And2_resultIsLoose()
 {
-	CSVParser testRead("testfiles/defaultlevel");
+	CSVParser testRead("testfiles/testlevel");
 
 	CPPUNIT_ASSERT_EQUAL((std::string)"loose", testRead.getTextInField(3,2));
 }
 
 void CSVParserTest::isFileCorrect_parseFileDefaultLevelError_resulIsFalse()
 {
-	CSVParser testRead("testfiles/defaultlevelerror");
+	CSVParser testRead("testfiles/testlevelonerowtoshort");
 
 	CPPUNIT_ASSERT(!testRead.isFileCorrect());
 }
