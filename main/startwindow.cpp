dcc991f11f2b32ded488c7a449058bf96bacc6bf
Markus Hofbauer <markus.hofbauer@tum.de>
2013-06-19 17:13:24 +0200

    add class and ui startwindow

---
+++ main/startwindow.cpp
@@ -0,0 +1,10 @@
+#include "startwindow.h"
+#include "common/uniqueidcreator.h"
+#include "common/leveldefinition.h"
+
+using namespace Common;
+using namespace std;
+
+StartWindow::StartWindow()
+{
+}
---
+++ main/startwindow.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_STARTWINDOW_H
+#define MAIN_STARTWINDOW_H
+
+class QGraphicsView;
+
+class StartWindow
+{
+public:
+	StartWindow();
+};
+
+#endif // STARTWINDOW_H
