e766fd717d169176fb7ffa0637596b48f0e44b25
Florian Wirnshofer <florian.wirnshofer@hotmail.de>
2013-06-19 10:58:49 +0200

    added test

--- common/powerupmaxbombrangestatetest.h
+++ common/powerupmaxbombrangestatetest.h
@@ -1,4 +1,27 @@
-#ifndef POWERUPMAXBOMBRANGESTATETEST_H
-#define POWERUPMAXBOMBRANGESTATETEST_H
+#ifndef COMMON_POWERUPMAXBOMBRANGESTATETEST_H
+#define COMMON_POWERUPMAXBOMBRANGESTATETEST_H
 
-#endif // POWERUPMAXBOMBRANGESTATETEST_H
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+namespace Common
+{
+class PowerUpMaxBombRangeStateTest :
+        public CPPUNIT_NS::TestFixture
+{
+    CPPUNIT_TEST_SUITE(PowerUpMaxVelocityStateTest);
+    CPPUNIT_TEST(modifyPlayer_maxSpeed_7);
+    CPPUNIT_TEST(modifyPlayer_baseClass_maxSpeed_7);
+    CPPUNIT_TEST_SUITE_END();
+
+private:
+    void modifyPlayer_maxSpeed_7();
+    void modifyPlayer_baseClass_maxSpeed_7();
+
+};
+}
+
+
+
+
+#endif
