811888013678ae28877498e7a700d3954e1c8daf
Benedikt Schmidt <benediktibk@aon.at>
2013-06-21 19:40:44 +0200

    added test for constructor of router

--- gameengine/routertest.cpp
+++ gameengine/routertest.cpp
@@ -1,4 +1,49 @@
 #include "gameengine/routertest.h"
 #include "gameengine/router.h"
+#include "gameengine/grid.h"
+#include "common/leveldefinition.h"
+#include "common/gamestate.h"
+#include "common/uniqueidcreator.h"
 
 using namespace GameEngine;
+using namespace Common;
+
+void RouterTest::constructor_validGrid_gridHasOneObserver()
+{
+	CPPUNIT_ASSERT_EQUAL((size_t)1, m_grid->getObserverCount());
+}
+
+void RouterTest::setUp()
+{
+	m_playerIdCreator = new UniqueIdCreator();
+	m_wallIdCreator = new UniqueIdCreator();
+	LevelDefinition level(15, 10);
+	level.setObjectTypeAtPosition(LevelDefinition::ObjectTypePlayer, 7, 8);
+	createRouter(level);
+}
+
+void RouterTest::tearDown()
+{
+	delete m_router;
+	m_router = 0;
+	delete m_gameState;
+	m_gameState = 0;
+	delete m_grid;
+	m_grid = 0;
+	delete m_wallIdCreator;
+	m_wallIdCreator = 0;
+	delete m_playerIdCreator;
+	m_playerIdCreator = 0;
+}
+
+void RouterTest::createRouter(const LevelDefinition &level)
+{
+	delete m_router;
+	delete m_grid;
+	delete m_gameState;
+
+	m_grid = new Grid(level.getLevelHeight(), level.getLevelWidth());
+	m_gameState = new GameState(level, level.getPlayerStartPositionCount(), *m_playerIdCreator, *m_wallIdCreator);
+	m_router = new Router(*m_grid, *m_gameState);
+}
+
--- gameengine/routertest.h
+++ gameengine/routertest.h
@@ -4,15 +4,41 @@
 #include <cppunit/TestFixture.h>
 #include <cppunit/extensions/HelperMacros.h>
 
+namespace Common
+{
+	class LevelDefinition;
+	class GameState;
+	class UniqueIdCreator;
+}
+
 namespace GameEngine
 {
+	class Grid;
+	class Router;
+
 	class RouterTest :
 			public CPPUNIT_NS::TestFixture
 	{
 		CPPUNIT_TEST_SUITE(RouterTest);
+		CPPUNIT_TEST(constructor_validGrid_gridHasOneObserver);
 		CPPUNIT_TEST_SUITE_END();
 
+	public:
+		virtual void setUp();
+		virtual void tearDown();
+
 	private:
+		void constructor_validGrid_gridHasOneObserver();
+
+	private:
+		void createRouter(const Common::LevelDefinition &level);
+
+	private:
+		Grid *m_grid;
+		Common::UniqueIdCreator *m_playerIdCreator;
+		Common::UniqueIdCreator *m_wallIdCreator;
+		Router *m_router;
+		Common::GameState *m_gameState;
 	};
 }
 
