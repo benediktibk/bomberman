36a4469ecce1e5d9d6f0fcf5b10f4d336de543bf
Kevin Meyer <meyer.kevin@mytum.de>
2013-06-26 13:25:23 +0200

    improved playerInfo String.

--- main/mainwindow.cpp
+++ main/mainwindow.cpp
@@ -137,10 +137,12 @@ void MainWindow::updatePlayerStateInfo()
 		assert(false);
 
 	QString messageString("");
-	QString templateString("P%1 B:%2 R:%3 | ");
+	QString templateString("P%1 B:%2 R:%3");
 
 	for(size_t y = 0; y < playerInformation.size() / 2; y++)
 	{
+		if (y!=0)
+			messageString += QString("  |  ");
 		messageString += QString(templateString.arg(QString().number(y + 1), QString().number(playerInformation.at(y)), QString().number(playerInformation.at(y+1))));
 	}
 
