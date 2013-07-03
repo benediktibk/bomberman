2db0a34f8c07b7a1042ce237a4e5a1bf8a37ed61
Markus Hofbauer <markus.hofbauer@tum.de>
2013-07-03 09:12:17 +0200

    add header with enum winnertype

---
+++ common/winnnertype.h
@@ -0,0 +1,15 @@
+#ifndef COMMON_WINNNERTYPE_H
+#define COMMON_WINNNERTYPE_H
+
+namespace Common
+{
+	enum WinnerType
+	{
+		WinnerTypePlayer1,
+		WinnerTypePlayer2,
+		WinnerTypeKI,
+		WinnerTypeDraw
+	};
+}
+
+#endif // WINNNERTYPE_H
