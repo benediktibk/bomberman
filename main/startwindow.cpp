cfd88b62c9f7b1028ba239225a6dbcfdf8e281bd
Markus Hofbauer <markus.hofbauer@tum.de>
2013-07-03 18:25:02 +0200

    change control to tab

--- main/startwindow.cpp
+++ main/startwindow.cpp
@@ -168,7 +168,7 @@ void StartWindow::controlClicked()
 {
 	QMessageBox messageBox;
 	messageBox.setWindowTitle("Control");
-	messageBox.setText("Player 1:\trun:\tarrow keys \n\tbomb:\tspace\nPlayer 2:\trun:\tW A S D \n\tbomb:\tQ\nStetting:\tMute:\tM\n\tPause:\tP");
+	messageBox.setText("Player 1:\trun:\tArrow keys \n\tbomb:\tSpace\nPlayer 2:\trun:\tW A S D \n\tbomb:\tTab\nStetting:\tMute:\tM\n\tPause:\tP");
 	messageBox.exec();
 }
 
